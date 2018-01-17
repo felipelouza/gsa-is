6cb6d22b3390534c9f271c54edf96c518e8ceb5f
Felipe A. Louza <felipelouza@gmail.com>
2018-01-17 09:49:49 -0200

    Fixed small bug (getline)

--- experiments/lib/file.c
+++ experiments/lib/file.c
@@ -114,9 +114,8 @@ char** load_multiple_txt(FILE* f_in, int k, int_t *n) {
 
 	int i;
  	for(i=0; i<k; i++){
-		size_t len = 0;
-		c_buffer[i] = NULL;
-			
+
+		size_t len = 0; c_buffer[i] = NULL;		
 		ssize_t size = getline(&c_buffer[i], &len, f_in);
 		if (size == -1){
 			printf("K = %d\n", i);
@@ -136,22 +135,23 @@ char** load_multiple_fastq(FILE* f_in, int k, int_t *n){
 
 	char **c_buffer = (char**) malloc(k*sizeof(char*));
 
+  size_t len = 0;
+	char *buf = NULL;
 	int i;
  	for(i=0; i<k; i++){
 
-                size_t len = 0;
-		char *buf = NULL;
-
+		len = 0; buf = NULL;
 		ssize_t size = getline(&buf, &len, f_in); // @'s line
-                if (size <= 1)
-                        return 0;
+    if (size <= 1)
+    return 0;
 
-                c_buffer[i] = NULL;
+		len = 0; c_buffer[i] = NULL;
 		size = getline(&c_buffer[i], &len, f_in); // read line
-                c_buffer[i][size-1] = 0;
+    c_buffer[i][size-1] = 0;
 
-                (*n) += size;
+    (*n) += size;
 
+		len = 0; buf = NULL;
 		getline(&buf, &len, f_in); // +'s line
 		getline(&buf, &len, f_in); // @'s line
 
@@ -178,11 +178,11 @@ char** load_multiple_fasta(FILE* f_in, int k, int_t *n){
 
 		if(i!=count) return 0;
 
-		len = 0;
 		int nalloc = 128;
 		c_buffer[i] = malloc(nalloc*sizeof(char));
 
 		int_t p=0;
+		len = 0; buf=NULL;
 		while(getline(&buf, &len, f_in)!=-1){
 
 			if(buf[0] == '>'){
@@ -283,11 +283,11 @@ return 1;
 
 int_t file_text_read(unsigned char** str, char* c_file, const char* ext){
 
-        FILE *f_in;
-        char *c_in = malloc((strlen(c_file)+strlen(ext))*sizeof(char));
-
-        sprintf(c_in, "%s.%s", c_file, ext);
-        f_in = file_open(c_in, "rb");
+	FILE *f_in;
+	char *c_in = malloc((strlen(c_file)+strlen(ext))*sizeof(char));
+	
+	sprintf(c_in, "%s.%s", c_file, ext);
+	f_in = file_open(c_in, "rb");
 
 	fseek(f_in, 0L, SEEK_END);
 	size_t size = ftell(f_in);
@@ -295,22 +295,22 @@ int_t file_text_read(unsigned char** str, char* c_file, const char* ext){
 
 	int_t n = size/sizeof(unsigned char);
 
-        *str = (unsigned char*) malloc(n*sizeof(unsigned char));
-        fread(*str, sizeof(unsigned char), n, f_in);
-
-        file_close(f_in);
-        free(c_in);
+	*str = (unsigned char*) malloc(n*sizeof(unsigned char));
+	fread(*str, sizeof(unsigned char), n, f_in);
+	
+	file_close(f_in);
+	free(c_in);
 
 return n;
 }
 
 int_t file_text_int_read(int_t** str_int, char* c_file, const char* ext){
 
-        FILE *f_in;
-        char *c_in = malloc((strlen(c_file)+strlen(ext))*sizeof(char));
-
-        sprintf(c_in, "%s.%s", c_file, ext);
-        f_in = file_open(c_in, "rb");
+	FILE *f_in;
+	char *c_in = malloc((strlen(c_file)+strlen(ext))*sizeof(char));
+	
+	sprintf(c_in, "%s.%s", c_file, ext);
+	f_in = file_open(c_in, "rb");
 
 	fseek(f_in, 0L, SEEK_END);
 	size_t size = ftell(f_in);
@@ -318,11 +318,11 @@ int_t file_text_int_read(int_t** str_int, char* c_file, const char* ext){
 
 	int_t n = size/sizeof(int_t);
 
-        *str_int = (int_t*) malloc(n*sizeof(int_t));
-        fread(*str_int, sizeof(int_t), n, f_in);
-
-        file_close(f_in);
-        free(c_in);
+	*str_int = (int_t*) malloc(n*sizeof(int_t));
+	fread(*str_int, sizeof(int_t), n, f_in);
+	
+	file_close(f_in);
+	free(c_in);
 
 return n;
 }
